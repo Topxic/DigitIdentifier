Fixed numerical errors in activation functions; Initial weights are set properly; Randomly transformed digits have better impact on network training; Added momentum; Training data can be read/written from/to file in batches

activation.py
@@ -1,40 +1,51 @@
 import numpy as np
 
+
 class Activation: 
-    def function(input: np.ndarray) -> np.ndarray:
+    def function(signal: np.ndarray) -> np.ndarray:
         pass
 
-    def derivative(input: np.ndarray) -> np.ndarray:
+    def derivative(signal: np.ndarray) -> np.ndarray:
         pass
 
 
 class SigmoidActivation(Activation):
-    def function(input: np.ndarray) -> np.ndarray:
-        return 1 / (1 + np.exp(-input))
+    def function(signal: np.ndarray) -> np.ndarray:
+        signal = np.clip(signal, -500, 500)
+        return 1 / (1 + np.exp(-signal))
         
-    def derivative(input: np.ndarray) -> np.ndarray:
-        s = SigmoidActivation.function(input)
+    def derivative(signal: np.ndarray) -> np.ndarray:
+        signal = np.clip(signal, -500, 500)
+        s = SigmoidActivation.function(signal)
         return s * (1 - s)
 
 
 class SoftmaxActivation(Activation):
-    def function(input: np.ndarray) -> np.ndarray:
-        return np.exp(input) / np.sum(np.exp(input))
+    def function(signal: np.ndarray) -> np.ndarray:
+        signal = np.clip(signal, -500, 500)
+        exp = np.exp(signal)
+        return exp / np.sum(exp)
         
-    def derivative(input: np.ndarray) -> np.ndarray:
-        expSum = np.sum(np.exp(input))
-        exp = np.exp(input)
-        return (exp * expSum - exp ** 2) / (expSum ** 2)
+    def derivative(signal: np.ndarray) -> np.ndarray:
+        signal = np.clip(signal, -500, 500)
+        exp = np.exp(signal)
+        expSum = np.sum(exp)
+        denom = expSum ** 2
+        if np.abs(denom) < 1.e-16:
+            denom = np.sign(denom) * 1.e-16
+        return (exp * expSum - exp ** 2) / denom
     
 
 class ReLUActivation(Activation):
-    def function(input: np.ndarray) -> np.ndarray:
-        return np.maximum(0, input)
+    def function(signal: np.ndarray) -> np.ndarray:
+        signal = np.clip(signal, -500, 500)
+        return np.maximum(0, signal)
         
-    def derivative(input: np.ndarray) -> np.ndarray:
-        result = np.zeros(shape=len(input))
-        for i in range(len(input)):
-            if input[i] > 0:
+    def derivative(signal: np.ndarray) -> np.ndarray:
+        signal = np.clip(signal, -500, 500)
+        result = np.zeros(shape=len(signal))
+        for i in range(len(signal)):
+            if signal[i] > 0:
                 result[i] = 1
         return result
     

cost.py
@@ -4,7 +4,7 @@ class Cost:
     def function(expected: np.ndarray, actual: np.ndarray) -> float:
         pass
 
-    def derivative(expected: np.ndarray, actual: np.ndarray) -> float:
+    def derivative(expected: np.ndarray, actual: np.ndarray) -> np.ndarray:
         pass
 
 
@@ -14,6 +14,6 @@ class MeanSquareCost(Cost):
         return np.sum(np.power(actual - expected, 2)) / len(actual)
         
 
-    def derivative(expected: np.ndarray, actual: np.ndarray) -> float:
+    def derivative(expected: np.ndarray, actual: np.ndarray) -> np.ndarray:
         assert len(actual) == len(expected)
         return expected - actual

layer.py
@@ -6,11 +6,13 @@ from mnist import *
 
 class Layer:
     def __init__(self, numInputNodes: int, numOutputNodes: int, activation: Activation):
-        self.weights = np.random.rand(numOutputNodes, numInputNodes) - 0.5
-        self.bias = np.random.rand(numOutputNodes) - 0.5
+        self.weights = np.random.randn(numOutputNodes, numInputNodes) / np.sqrt(numInputNodes)
+        self.bias = np.zeros(numOutputNodes)
         self.activation = activation
         self.biasGradient = np.zeros(shape=(numOutputNodes))
         self.weightGradient = np.zeros(shape=(numOutputNodes, numInputNodes))
+        self.prevBiasGradient = np.zeros(shape=(numOutputNodes))
+        self.prevWeightGradient = np.zeros(shape=(numOutputNodes, numInputNodes))
 
     def setWeightsAndBiase(self, weights: np.ndarray, bias: np.ndarray):
         assert self.weights.shape == weights.shape
@@ -36,9 +38,12 @@ class Layer:
         self.weightGradient += np.outer(delta, prevLayerActivation)
         return delta
 
-    def applyGradient(self, learningRate, batchSize):
-        learningFactor = learningRate / batchSize
-        self.bias += learningFactor * self.biasGradient
-        self.weights += learningFactor * self.weightGradient
+    def applyGradient(self, learningRate, batchSize, momentum):
+        self.biasGradient /= batchSize
+        self.weightGradient /= batchSize
+        self.bias += (1 - momentum) * learningRate * self.biasGradient + momentum * self.prevBiasGradient
+        self.weights += (1 - momentum) * learningRate * self.weightGradient + momentum * self.prevWeightGradient
+        self.prevBiasGradient = np.copy(self.biasGradient) 
+        self.prevWeightGradient = np.copy(self.weightGradient) 
         self.biasGradient.fill(0)
         self.weightGradient.fill(0)

mnist.py
@@ -1,8 +1,18 @@
-from matplotlib import pyplot as plt
+import os
 import numpy as np
+import pickle as pk
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button
+
+SCALE_RANGE = (0.7, 1.1)
+ROTATION_RANGE = (-20, 20)
+TRANSLATION_RANGE = (-5, 5)
+NOISE_RANGE = (0.05, 0.1)
+
+FILE_NAME = 'mnist-training-data.pkl'
 
 class MNISTSample:
-    def __init__(self, image, label):
+    def __init__(self, image: np.ndarray, label: int):
         self.image = image
         self.label = label
         self.imageVec = image.flatten() / 255
@@ -15,8 +25,9 @@ class MNISTSample:
 
         rad = np.radians(rotation)
         c, s = np.cos(rad), np.sin(rad)
-        T = np.array([[c*scale, -s*scale, translation[0]],
-                      [s*scale, c*scale, translation[1]],
+        scale = 1 / scale
+        T = np.array([[c*scale, -s*scale, translation[1]],
+                      [s*scale, c*scale, translation[0]],
                       [0, 0, 1]])
 
         outputImage = np.zeros_like(self.image)
@@ -28,13 +39,22 @@ class MNISTSample:
                 if 0 <= x < rows and 0 <= y < cols:
                     outputImage[r, c] = self.image[x, y]
 
-        noiseImage = noise * np.random.rand(rows, cols)
+        noiseImage = noise * np.random.randn(rows, cols) * 255
 
-        return np.minimum(255, outputImage + noiseImage)
+        return np.clip(outputImage + noiseImage, 0, 255)
+    
+    def transfromRandomly(self):
+        scale = np.random.uniform(SCALE_RANGE[0], SCALE_RANGE[1])
+        rotation = np.random.uniform(ROTATION_RANGE[0], ROTATION_RANGE[1])
+        translationX = np.random.uniform(TRANSLATION_RANGE[0], TRANSLATION_RANGE[1])
+        translationY = np.random.uniform(TRANSLATION_RANGE[0], TRANSLATION_RANGE[1])
+        noise = np.random.uniform(NOISE_RANGE[0], NOISE_RANGE[1])
+        image = self.transform(scale, rotation, np.asarray([translationX, translationY]), noise)
+        return MNISTSample(image, self.label)
 
 
 # Data provided by http://yann.lecun.com/exdb/mnist/
-def parseTrainingData():
+def parseTrainingData() -> list[MNISTSample]:
     with open("./train-images.idx3-ubyte", "rb") as f:
         magic_number = int.from_bytes(f.read(4), byteorder="big")
         image_count = int.from_bytes(f.read(4), byteorder="big")
@@ -54,7 +74,7 @@ def parseTrainingData():
     return trainingSamples
 
 
-def parseTestData():
+def parseTestData() -> list[MNISTSample]:
     with open("./t10k-images.idx3-ubyte", "rb") as f:
         magic_number = int.from_bytes(f.read(4), byteorder="big")
         image_count = int.from_bytes(f.read(4), byteorder="big")
@@ -72,3 +92,172 @@ def parseTestData():
     for image, label in zip(images, labels):
         testSamples += [MNISTSample(image, label)]
     return testSamples
+
+
+def exploreTransformationParams():
+    """ 
+    Used for adjusting random transformation parameters 
+    such that every digit is still recognizable
+    """
+
+    data = parseTrainingData()
+    imgIdx = 0
+    initScale = 1
+    initRotation = 0
+    initTranslationX = 0
+    initTranslationY = 0
+    initNoise = 0
+
+    # Create the figure and the line that we will manipulate
+    fig, ax = plt.subplots()
+    imageContainer = plt.imshow(data[imgIdx].image, cmap='gray')
+    fig.subplots_adjust(bottom=0.35)
+
+    axfreq1 = fig.add_axes([0.25, 0.05, 0.65, 0.03])
+    scaleSlider = Slider(
+        ax=axfreq1,
+        label='Scale',
+        valmin=SCALE_RANGE[0],
+        valmax=SCALE_RANGE[1],
+        valinit=initScale,
+    )
+
+    axfreq2 = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+    rotationSlider = Slider(
+        ax=axfreq2,
+        label='Rotation',
+        valmin=ROTATION_RANGE[0],
+        valmax=ROTATION_RANGE[1],
+        valinit=initRotation,
+    )
+
+    axfreq3 = fig.add_axes([0.25, 0.15, 0.65, 0.03])
+    translationXSlider = Slider(
+        ax=axfreq3,
+        label='Translation X',
+        valmin=TRANSLATION_RANGE[0],
+        valmax=TRANSLATION_RANGE[1],
+        valinit=initTranslationX,
+    )
+
+    axfreq4 = fig.add_axes([0.25, 0.2, 0.65, 0.03])
+    translationYSlider = Slider(
+        ax=axfreq4,
+        label='Translation Y',
+        valmin=TRANSLATION_RANGE[0],
+        valmax=TRANSLATION_RANGE[1],
+        valinit=initTranslationY,
+    )
+
+    axfreq5 = fig.add_axes([0.25, 0.25, 0.65, 0.03])
+    noiseSlider = Slider(
+        ax=axfreq5,
+        label='Noise',
+        valmin=NOISE_RANGE[0],
+        valmax=NOISE_RANGE[1],
+        valinit=initNoise,
+    )
+
+
+    # The function to be called anytime a slider's value changes
+    def update(val):
+        translation = np.asarray([translationXSlider.val, translationYSlider.val])
+        transformedImage = data[imgIdx].transform(
+            scaleSlider.val,
+            rotationSlider.val, 
+            translation,
+            noiseSlider.val
+        )
+        imageContainer.set_data(transformedImage)
+        plt.draw()
+
+    # register the update function with each slider
+    scaleSlider.on_changed(update)
+    rotationSlider.on_changed(update)
+    translationXSlider.on_changed(update)
+    translationYSlider.on_changed(update)
+    noiseSlider.on_changed(update)
+
+    # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
+    resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+    button = Button(resetax, 'Reset', hovercolor='0.975')
+
+
+    def reset(event):
+        scaleSlider.reset()
+        rotationSlider.reset()
+        translationXSlider.reset()
+        translationYSlider.reset()
+        noiseSlider.reset()
+    button.on_clicked(reset)
+
+    plt.show()
+
+
+def plotRandomTransformations():
+    data = parseTrainingData()
+    imgIdx = 0
+
+    fig, _ = plt.subplots()
+    imageContainer = plt.imshow(data[imgIdx].image, cmap='gray')
+    fig.subplots_adjust(bottom=0.15)
+
+    nextSampleAxis = fig.add_axes([0.2, 0.025, 0.25, 0.04])
+    nextSampleButton = Button(nextSampleAxis, 'Next sample', hovercolor='0.975')
+
+    randoransformationAxis = fig.add_axes([0.5, 0.025, 0.3, 0.04])
+    randoransformationButton = Button(randoransformationAxis, 'Random transformation', hovercolor='0.975')
+
+    def nextSample(event):
+        nonlocal imgIdx
+        imgIdx += 1
+        imageContainer.set_data(data[imgIdx].image)
+        plt.draw()
+
+
+    def nextTransformation(event):
+        imageContainer.set_data(data[imgIdx].transfromRandomly().image)
+        plt.draw()
+
+
+    nextSampleButton.on_clicked(nextSample)
+    randoransformationButton.on_clicked(nextTransformation)
+
+    plt.show()
+
+
+def mnist2file(transformationsPerSample):
+    if os.path.isfile(FILE_NAME): 
+        return
+    
+    data = parseTrainingData() + parseTestData()
+    with open(FILE_NAME, 'wb') as file:
+        # Save number of samples
+        pk.dump(transformationsPerSample * len(data), file)
+
+        # Generate and save randomly transformed samples
+        counter = 1
+        for sample in data:
+            for _ in range(transformationsPerSample):
+                transformed = sample.transfromRandomly()
+                pk.dump(transformed.image, file)
+                pk.dump(transformed.label, file)
+            print(f'\rGenerating data {counter / len(data) * 100:.3f}%', end='')
+            counter += 1
+    print()
+
+
+def file2mnist(batchSize):
+    with open(FILE_NAME, 'rb') as file:
+        numSamples = pk.load(file)
+        batch = []
+        counter = 0
+        for _ in range(numSamples):
+            image = pk.load(file)
+            label = pk.load(file)
+            batch += [MNISTSample(image, label)]
+            counter += 1
+
+            if len(batch) == batchSize:
+                yield batch, counter / numSamples
+                batch = []

training.py
@@ -7,14 +7,12 @@ from mnist import *
 from layer import Layer
 
 
-def backpropagation(layers: list[Layer], trainingData, testData, learningRate, batchSize, epochs, costFunction):
-    numTrainingData = len(trainingData)
-    numBatches = numTrainingData // batchSize
-    batches = [trainingData[x*batchSize:x*batchSize+batchSize] for x in range(numBatches)]
+def backpropagation(layers: list[Layer], trainingDataGenerator, testData: list[MNISTSample], 
+                    learningRate: float, batchSize: int, epochs: int, costFunction: Cost, momentum: float):
 
     for epoch in range(1, epochs + 1):
-        counter = 1
-        for batch in batches:
+        counter = 0
+        for batch, progress in trainingDataGenerator(batchSize):
             averageBatchError = 0
             # Perform backpropagation
             for sample in batch:
@@ -37,12 +35,12 @@ def backpropagation(layers: list[Layer], trainingData, testData, learningRate, b
 
                 averageBatchError += costFunction.function(expected, activations[-1])
             
-            # Apply gradient and print statistics
+            # Apply gradient
             for layer in layers:
-                layer.applyGradient(learningRate, batchSize)
+                layer.applyGradient(learningRate, batchSize, momentum)
             averageBatchError /= len(batch)
-            print(f'\rEpoch: {epoch}: {counter / numBatches * 100:.3f}%, average cost: {averageBatchError:.6f}', end='')
-            counter += 1
+            counter += len(batch)
+            print(f'\rEpoch: {epoch} - {progress * 100:.3f}% average loss: {averageBatchError:.6f}', end='')
 
     # Evaluate network accuracy
     correct = 0
@@ -53,3 +51,4 @@ def backpropagation(layers: list[Layer], trainingData, testData, learningRate, b
         if np.argmax(x) == sample.label:
             correct += 1
     print(f'\nNetwork accuracy: {correct / len(testData) * 100:.3f}%')
+

ui.py
@@ -6,44 +6,26 @@ from layer import *
 from mnist import *
 from training import *
 
-# Create network training data and train
-trainingData = parseTrainingData()
-scaleRange=(0.2, 1.1)
-rotationRange=(-45, 45)
-translationRange=(-5, 5)
-noiseRange=(0, 0.8)
-transformedTrainingData = []
-counter = 1
-for sample in trainingData[:1000]:
-    for _ in range(1):
-        scale = np.random.uniform(scaleRange[0], scaleRange[1])
-        rotation = np.random.uniform(rotationRange[0], rotationRange[1])
-        translation = np.random.uniform(translationRange[0], translationRange[1], size=2)
-        noise = np.random.uniform(noiseRange[0], noiseRange[1])
-        transformedImage = sample.transform(scale, rotation, translation, noise)
-        transformedTrainingData += [MNISTSample(transformedImage, sample.label)]
-    print(f'\rGenerating data: {counter / len(trainingData[:1000]) * 100:.3f}%', end='')
-    counter += 1
-trainingData += transformedTrainingData
+
+# Generate training data
+mnist2file(10)
 testData = parseTestData()
-
+# Create and train network
 network = [
-    Layer(784, 10, SigmoidActivation)
+    Layer(784, 200, ReLUActivation),
+    Layer(200, 10, SoftmaxActivation)
 ]
-backpropagation(network, trainingData, testData, 0.01, 1, 3, MeanSquareCost)
-
+backpropagation(network, file2mnist, testData, 0.01, 32, 3, MeanSquareCost, 0.9)
+result = np.zeros(10)
 
 # Configuration
 scale = 10
 size = np.array([28, 28])
 window_size = np.array([scale * 28 + 170, scale * 28])
 canvas = np.zeros(size)
-
 white = np.array([255, 255, 255])
 red = np.array([255, 0, 0])
 black = np.array([0, 0, 0])
-
-result = np.zeros(10)
 
 # Create GUI
 pygame.init()